0fcb55b7a8914c82a8e6dfb320af3365d84f597d
Gazstao <gazstao@air-de-gazstao.intranet>
2020-04-28 12:45:18 -0300

    updated

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 $(".corpoClinico").click(function(){window.location = "corpoClinico.html";});
 $(".clinica").click(function(){window.location = "aClinica.html";});
+$(".contato").click(function(){window.location = "index.html#contato";});
