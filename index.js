d5c4adfd1f120e3043b92fb5349f8d29407f8960
Gazstao <gazstao@MacBook-Air-de-Gazstao.local>
2020-04-30 10:49:37 -0300

    updated

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 $(".corpoClinico").click(function(){window.location = "corpoClinico.html";});
 $(".clinica").click(function(){window.location = "aClinica.html";});
 $(".contato").click(function(){window.location = "index.html#contato";});
+$(".convenios").click(function(){window.location = "convenios.html";});
